3f7afcd40d0d97a7997ce09dcdef1ef4c2d3bafa
mccoogp <mccoogp@icloud.com>
2025-02-13 18:28:11 -0800

    Added popularity in menu

--- district_menu.gd
+++ district_menu.gd
@@ -1,12 +1,14 @@
 extends Sprite2D
 
 
-var industry_clicked = false
-
+var industry_clicked:bool = false
+var popularity_clicked:bool = false
 var anyclicked = false
 func _process(delta: float) -> void:
 	var click = false
 	if industry_clicked == true:
+		click = true
+	if popularity_clicked == true:
 		click = true
 	if click == false:
 		anyclicked = false
---
+++ popularity.gd
@@ -0,0 +1,17 @@
+extends Sprite2D
+
+@export var menu: Node2D
+
+func _input(event):
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+			if is_pixel_opaque(get_local_mouse_position()):
+				print("here")
+				menu.popularity_clicked = true
+				print("here")
+				menu.anyclicked = true
+				print("here")
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT and event.is_released():
+			if is_pixel_opaque(get_local_mouse_position()):
+				menu.popularity_clicked = false
